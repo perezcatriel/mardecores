456caa67ec0e6a045916b37d179557d36d87b925
Catriel Pérez <perezcatriel@gmail.com>
2022-10-12 22:06:48 -0300

    Agregando iconos

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,49 +3,49 @@ import "./Contact.css";
 function Contact() {
 	return (
 		<section className="contact">
-			<article className="contact--contact">
-				<h2 className="contact-h2">Contact</h2>
+			<article className="contact--contact-1">
+				<h2 className="contact--contact__title">Contact</h2>
 
-				<div>
+				<div className="contact--info-1">
 					<p>500 Terry Francois Street</p>
 					<p>San Francisco, CA 94158</p>
 				</div>
 
-				<div>
+				<div className="contact--info-1">
 					<a>Mail: info@my-domain.com</a>
 					<a>Tel: 123-456-7890</a>
 				</div>
 			</article>
 
-			<article className="contact--contact2">
-				<h2 className="contact-h2">Contact</h2>
+			<article className="contact--contact-2">
+				<h2 className="contact--contact__title">Contact</h2>
 
-				<div>
+				<div className="contact--info-1">
 					<p>500 Terry Francois Street</p>
 					<p>San Francisco, CA 94158</p>
 				</div>
 
-				<div>
+				<div className="contact--info-1">
 					<a>Mail: info@my-domain.com</a>
 					<a>Tel: 123-456-7890</a>
 				</div>
 			</article>
 
 			<article className="contact--menu">
-				<h2 className="contact-h2">Shop</h2>
-				<h2 className="contact-h2">Collections</h2>
-				<h2 className="contact-h2">Philosophy</h2>
+				<h2 className="contact--contant__title">Shop</h2>
+				<h2 className="contact--contant__title">Collections</h2>
+				<h2 className="contact--contant__title">Philosophy</h2>
 			</article>
 
 			<article className="contact--redes">
-				<h2 className="contact-h2">Follow us</h2>
+				<h2 className="contact--contant__title">Follow us</h2>
 
 				<ul className="contact--redes__lists">
 					<li>Facebook</li>/<li>Instagam</li>/<li>Youtube</li>
 				</ul>
 
 				<div className="contact--redes__by">
-					<p>© 2023 By Meshi Yoga. Proudly created with Wix.com</p>
+					<p>© 2022 By Mar de Cores created for <a href="https://perezcatriel.github.io/cv/public">Catriel Pérez</a></p>
 				</div>
 			</article>
 		</section>
--- src/components/Registrer.js
+++ src/components/Registrer.js
@@ -3,15 +3,15 @@ import "./Register.css";
 function Registrer() {
 	return (
 		<section className="register">
-			<p className="register--parrafo">Register</p>
+			{/* <p className="register--parrafo">Register</p> */}
 			<form className="register--form">
 				<input
 					type="email"
 					placeholder="name@gmail.com"
 					className="register--form__input"
 				/>
-				<button type="button" className="rregister--form__button">
-					Join
+				<button type="button" className="register--form__button">
+					Register
 				</button>
 			</form>
 		</section>
