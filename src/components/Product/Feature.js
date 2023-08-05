510df2aef23842167069fef9d857165eef0cd51d
perezcatriel <perezcatriel@gmail.com>
2023-08-05 14:31:23 -0300

    new images and styles

--- src/components/Product/Feature.js
+++ src/components/Product/Feature.js
@@ -4,36 +4,36 @@ import bikini from "../../assets/bikini.jpeg";
 function Feature() {
 	return (
 		<section className="feature">
-			<h2 className="feature--title">Featured Products</h2>
+			<h2 className="feature--title">Productos Destacados</h2>
 
 			<article className="feature--cards">
 				<figure className="feature--card">
 					<img src={bikini} alt="image" className="feature--card__image" />
-					<caption className="feature--card__title">Viento Set</caption>
+					<caption className="feature--card__title">Pantalón</caption>
 					<figcaption className="feature--card__price">$10.00</figcaption>
 				</figure>
 
 				<figure className="feature--card">
 					<img src={bikini} alt="image" className="feature--card__image" />
-					<caption className="feature--card__title">Viento Set</caption>
+					<caption className="feature--card__title">Depilación Definitiva</caption>
 					<figcaption className="feature--card__price">$10.00</figcaption>
 				</figure>
 
 				<figure className="feature--card">
 					<img src={bikini} alt="image" className="feature--card__image" />
-					<caption className="feature--card__title">Viento Set</caption>
+					<caption className="feature--card__title">Tarot</caption>
 					<figcaption className="feature--card__price">$10.00</figcaption>
 				</figure>
 
 				<figure className="feature--card">
 					<img src={bikini} alt="image" className="feature--card__image" />
-					<caption className="feature--card__title">Viento Set</caption>
+					<caption className="feature--card__title">Bikini</caption>
 					<figcaption className="feature--card__price">$10.00</figcaption>
 				</figure>
 
 				<figure className="feature--card">
 					<img src={bikini} alt="image" className="feature--card__image" />
-					<caption className="feature--card__title">Viento Set</caption>
+					<caption className="feature--card__title">Hifu</caption>
 					<figcaption className="feature--card__price">$10.00</figcaption>
 				</figure>
 			</article>
