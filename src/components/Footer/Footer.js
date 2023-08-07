297f76ea4c04d826d3093c9719ca6bf1522f2ad3
perezcatriel <perezcatriel@gmail.com>
2023-08-07 13:57:22 -0300

    new logos

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,59 +4,33 @@ function Footer() {
 	return (
 		<section className="contact">
 			<article className="contact--contact-1">
-				<h2 className="contact--contact__title">Contact</h2>
+				<h2 className="contact--contact__title">Contáctanos</h2>
 
 				<div className="contact--info-1">
-					<p>500 Terry Francois Street</p>
-					<p>San Francisco, CA 94158</p>
+					<p>Av. San Martín, Cosquín, Argentina</p>
+					<p>C.P. 5166</p>
 				</div>
 
 				<div className="contact--info__anchor">
 					<a>Mail: info@my-domain.com</a>
 					<a>Tel: 123-456-7890</a>
 				</div>
-			</article>
-
-			<article className="contact--contact-2">
-				<h2 className="contact--contact__title">Contact</h2>
-
-				<div className="contact--info-1">
-					<p>500 Terry Francois Street</p>
-					<p>San Francisco, CA 94158</p>
-				</div>
-
-				<div className="contact--info__anchor">
-					<a>Mail: info@my-domain.com</a>
-					<a>Tel: 123-456-7890</a>
-				</div>
-			</article>
-
-			<article className="contact--menu">
-				<a>
-					<h2 className="contact--contant__title">Shop</h2>
-				</a>
-				<a>
-					<h2 className="contact--contant__title">Collections</h2>
-				</a>
-				<a>
-					<h2 className="contact--contant__title">Philosophy</h2>
-				</a>
 			</article>
 
 			<article className="contact--redes">
-				<h2 className="contact--redes__title">Follow us</h2>
+				<h2 className="contact--redes__title">Seguinos</h2>
 
 				<ul className="contact--redes__lists">
 					<a>
-						<li className="contact--redes__facebook">Facebook</li>
+						<li className="contact--redes__facebook">@marde_cores.ropa</li>
 					</a>
 					/
 					<a>
-						<li className="contact--redes__instagram">Instagam</li>
+						<li className="contact--redes__instagram">@marde_cores.estetica</li>
 					</a>
 					/
 					<a>
-						<li className="contact--redes__youtube">Youtube</li>
+						<li className="contact--redes__youtube">@marde_cores.magia</li>
 					</a>
 				</ul>
 
@@ -64,10 +38,10 @@ function Footer() {
 					<p>
 						© 2022 By Mar de Cores created for{" "}
 						<a
-							href="https://perezcatriel.github.io/cv/public"
+							href="https://datacraft.vercel.app/"
 							className="catriel"
 						>
-							Catriel Pérez
+							DataCraft
 						</a>
 					</p>
 				</div>
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,22 +1,23 @@
 import "./Promo.css";
-import bikini from "../../assets/icons/mardecores-caracol-azul.jpeg";
-import caracolNaranja from "../../assets/icons/mardecores-caracol-naranja.jpeg";
+import caracolAzul from "../../assets/icons/marde-caracol-azul.jpeg";
+import caracolAmarillo from "../../assets/icons/marde-caracol-amarillo.jpeg";
+import caracolRosa from "../../assets/icons/marde-caracol-rosa.jpeg";
 
 function Promo() {
 	return (
 		<section className="promo">
 			<figure className="promo--image">
-				<img src={bikini} alt="promo--image__img-1" className="promo--image__img-1" />
+				<img src={caracolAzul} alt="promo--image__img-1" className="promo--image__img-1" />
 				<p className="promo--image__parrafo">Ropa</p>
 			</figure>
 
 			<figure className="promo--image">
-				<img src={caracolNaranja} alt="promo--image__img-2" className="promo--image__img-2" />
+				<img src={caracolRosa} alt="promo--image__img-2" className="promo--image__img-2" />
 				<p className="promo--image__parrafo">Estética</p>
 			</figure>
 
 			<figure className="promo--image">
-				<img src={bikini} alt="promo--image__img-2" className="promo--image__img-2" />
+				<img src={caracolAmarillo} alt="promo--image__img-2" className="promo--image__img-2" />
 				<p className="promo--image__parrafo">Magia</p>
 			</figure>
 		</section>
